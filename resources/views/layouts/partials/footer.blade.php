6b3bd1068028583fc77070836b26b92218fb6250
Ammar Afzal <ammarafzal075@gmail.com>
2021-07-27 13:13:30 +0500

    Footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer text-center text-sm-left">
-    &copy; 2020 Dastone <span class="d-none d-sm-inline-block float-right">Crafted with <i class="mdi mdi-heart text-danger"></i> by Mannatthemes</span>
+    &copy; 2021 <span class="d-none d-sm-inline-block float-right">Al-Haram Furnitures</span>
 </footer><!--end footer-->
